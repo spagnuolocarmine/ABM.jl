a74b9c55bd2cdd14ed4a2f9229e1230b2bd24e2f
amrlnic <amoriello.nicola97@gmail.com>
2019-06-26 12:53:09 +0200

    create new methods and added execution logic
    -act
    -fstep

--- models/antsforage/ants.jl
+++ models/antsforage/ants.jl
@@ -13,6 +13,25 @@ global field = Field2D(width,height,neighborhood_distance/1.5,true)
 
 addfield!(simstate,field)
 
+function depositPheromone(state::SimState)
+    if hasFoodItem
+        max =
+    else
+        #body
+    end
+end
+
+function act()
+    #body
+end
+
+
+function fstep(state::SimState, agent::Agent)
+    depositPheromone(state)
+    act(state)
+end
+
+
 mutable struct AntData
     name::String
     pos::Real2D
@@ -21,11 +40,15 @@ mutable struct AntData
 
 end
 
-
-function depositPheromone()
-    if hasFoodItem
-        max =
-    else
-
-    end
+for i in 1:10000
+    initialReward = 0.0
+    pos = Real2D(rand(Uniform(0, width)), rand(Uniform(0, height)))
+    a = AntData("Ant", pos, initialReward, false)
+    ant = Agent(fstep,a)
+    setObjectLocation!(field, ant, pos)
+    #if i == 1
+        scheduleRepeating!(myschedule,boid)
+    #end
 end
+
+@time  simulate!(myschedule,10);
