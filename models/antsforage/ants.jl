1e935f8c9875c028231e24ffadd4ef5a0b073f9c
amrlnic <amoriello.nicola97@gmail.com>
2019-07-05 21:10:05 +0200

    file antsForages created

--- models/antsforage/ants.jl
+++ models/antsforage/ants.jl
@@ -3,41 +3,10 @@ using Revise
 using Distributions
 using BenchmarkTools
 
-simstate = SimState()
-myschedule = Schedule(simstate)
-width = 150.0
-height = 150.0
-neighborhood_distance = 0
-
-global field = Field2D(width,height,neighborhood_distance/1.5,true)
-
-
-#coordinates HOME
-global const HOME_X = 15.0
-global const HOME_Y = 15.0
-
-#coordinates FOOD
-global const FOOD_X = 135.0
-global const FOOD_Y = 135.0
-
-#values PHEROMONE
-global const IMPOSSIBLY_BAD_PHEROMONE = -1
-global const LIKELY_MAX_PHEROMONE = 3
-
-#POINT HOME AND FOOD
-pointHome = Real2D(HOME_X, HOME_Y)
-pointFood = Real2D(FOOD_X, FOOD_Y)
 
 Real2D last
 
-#MATRICES
-toFoodGrid = zeros(height, width)
-toHomeGrid = zeros(height, width)
 
-updateCutDown = 0.9
-diagonalCutDown = updateCutDown^√2
-
-addfield!(simstate,field)
 
 function depositPheromone(state::SimState, agent::Agent)
 
@@ -152,7 +121,42 @@ mutable struct AntData
 end
 
 
-pos = Real2D(HOME_X, HOME_Y)
+"""simstate = SimState()
+myschedule = Schedule(simstate)
+width = 150.0
+height = 150.0
+neighborhood_distance = 0
+
+global field = Field2D(width,height,neighborhood_distance/1.5,true)
+
+
+#coordinates HOME
+global const HOME_X = 15.0
+global const HOME_Y = 15.0
+
+#coordinates FOOD
+global const FOOD_X = 135.0
+global const FOOD_Y = 135.0
+
+#values PHEROMONE
+global const IMPOSSIBLY_BAD_PHEROMONE = -1
+global const LIKELY_MAX_PHEROMONE = 3
+
+#POINT HOME AND FOOD
+pointHome = Real2D(HOME_X, HOME_Y)
+pointFood = Real2D(FOOD_X, FOOD_Y)
+
+#MATRICES
+toFoodGrid = zeros(height, width)
+toHomeGrid = zeros(height, width)
+
+updateCutDown = 0.9
+diagonalCutDown = updateCutDown^√2
+
+addfield!(simstate,field)"""
+
+
+"""pos = Real2D(HOME_X, HOME_Y)
 initialReward = 0.0
 for i in 1:10000
     a = AntData("Ant", pos, initialReward, false)
@@ -161,4 +165,4 @@ for i in 1:10000
     scheduleRepeating!(myschedule,ant)
 end
 
-@time  simulate!(myschedule,10);
+@time  simulate!(myschedule,10);"""
