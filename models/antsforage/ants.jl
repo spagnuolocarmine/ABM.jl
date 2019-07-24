2573e279a315e4e87aa962626b00d0e758847c63
amrlnic <amoriello.nicola97@gmail.com>
2019-07-24 12:28:24 +0200

    fx ants.jl in favor of an Object Oriented structure

--- models/antsforage/ants.jl
+++ models/antsforage/ants.jl
@@ -4,49 +4,28 @@ using Distributions
 using BenchmarkTools
 using Parameters
 
-function depositPheromone(state::SimState, agent::Agent)
+function depositPheromone(state::SimState, agent::Agent, afd::AntsForageData)
 
-    location :: Real2D = getObjectLocation(field, agent)
+    currentfield = state.fields[length(state.fields)]
+    location :: Real2D = getObjectLocation(currentfield, agent)
 
     x::Int8 = location.x
     y::Int8 = location.y
 
     if agent.state.hasFoodItem
-        max = tofoodgrid[x, y]
+        max = afd.tofoodgrid[x, y]
         for dx = -1:1
             for dy = -1:1
                 _x = dx + x
                 _y = dy + y
 
-                if _x < 0 || _y < 0 || _x >= width || _y >= height
+                if _x < 0 || _y < 0 || _x >= currentfield.width || _y >= currentfield.height
                     continue
                 end
 
-                m = toFoodGrid[_x, _y]
+                m = afd.toFoodGrid[_x, _y]
                     *(dx * dy != 0 ?         #diagonal corners
-                    diagonalCutDown : updateCutDown) +
-                    reward
-
-                if m > max
-                    max = m
-                end
-            end
-        end
-        toFoodGrid[x, y] = max
-    else
-        max = toHomeGrid[x, y]
-        for dx = -1:1
-            for dy = -1:1
-                _x = dx + x
-                _y = dy + y
-
-                if _x < 0 || _y < 0 || _x >= width || _y >= height
-                    continue
-                end
-
-                m = toHomeGrid[_x, _y]
-                    *(dx * dy != 0 ?         #diagonal corners
-                    diagonalCutDown : updateCutDown) +
+                    afd.diagonalCutDown : afd.updateCutDown) +
                     agent.state.reward
 
                 if m > max
@@ -54,20 +33,44 @@ function depositPheromone(state::SimState, agent::Agent)
                 end
             end
         end
-        toHomeGrid[x, y] = max
+        afd.toFoodGrid[x, y] = max
+    else
+        max = afd.toHomeGrid[x, y]
+        for dx = -1:1
+            for dy = -1:1
+                _x = dx + x
+                _y = dy + y
+
+                if _x < 0 || _y < 0 || _x >= currentfield.width || _y >= currentfield.height
+                    continue
+                end
+
+                m = afd.toHomeGrid[_x, _y]
+                    *(dx * dy != 0 ?         #diagonal corners
+                    afd.diagonalCutDown : afd.updateCutDown) +
+                    agent.state.reward
+
+                if m > max
+                    max = m
+                end
+            end
+        end
+        afd.toHomeGrid[x, y] = max
     end
 
-    reward = 0.0
+    agent.state.reward = 0.0
 end
 
-function act(state::SimState, agent::Agent)
-    location = getObjectLocation(field, agent)
+function act(state::SimState, agent::Agent, afd::AntsForageData)
+
+    currentfield = state.fields[length(state.fields)]
+    location :: Real2D = getObjectLocation(currentfield, agent)
 
     x = location.x
     y = location.y
 
     if agent.state.hasFoodItem
-        max = IMPOSSIBLY_BAD_PHEROMONE
+        max = afd.IMPOSSIBLY_BAD_PHEROMONE
         max_x = x
         max_y = y
         count = 2
@@ -79,10 +82,10 @@ function act(state::SimState, agent::Agent)
 
                 if (dx == 0 & dy == 0) ||
                         _x < 0 || _y < 0 ||
-                        _x >= width || _y >= height
+                        _x >= currentfield.width || _y >= currentfield.height
                     continue
                 end
-                m = toHomeGrid[_x, _y]
+                m = afd.toHomeGrid[_x, _y]
                 if m > max
                     count = 2
                 end
@@ -93,12 +96,12 @@ function act(state::SimState, agent::Agent)
                 end
             end
         end
-        if max == 0 && lastPos != nothing              #nowhere to go!
+        if max == 0 && agent.state.lastPos != nothing              #nowhere to go!
             if rand(Bool)           #TODO SRISOLVERE RANDOM (rand(Bool) è provvisorio)
-                xm = x + (x - lastPos.x)
-                ym = y + (y - lastPos.y)
+                xm = x + (x - agent.state.lastPos.x)
+                ym = y + (y - agent.state.lastPos.y)
 
-                if xm >= 0 && xm < width && ym >= 0 && ym < height     #aggiungere ostacoli in futuro
+                if xm >= 0 && xm < currentfield.width && ym >= 0 && ym < currentfield.height     #aggiungere ostacoli in futuro
                     max_x = xm
                     max_y = ym
                 end
@@ -110,19 +113,24 @@ function act(state::SimState, agent::Agent)
             xm = x + xd
             ym = y + yd
 
-            if !(xd == 0 && yd == 0) && xm >= 0 && xm <= width && ym >= 0 && ym < height
+            if !(xd == 0 && yd == 0) && xm >= 0 && xm <= currentfield.width && ym >= 0 && ym < currentfield.height
                 max_x = xm;
                 max_y = ym;
             end
         end
-        setObjectLocation!(field, agent, Real2D(max_x, max_y))
+        setObjectLocation!(currentfield, agent, Real2D(max_x, max_y))
 
-        if getObjectsAtLocation(field, Real2D(max_x, max_y)) == HOME        #TODO manca if
-            reward = afReward
-            hasFoodItem = !hasFoodItem
+        objAtLoc = getObjectsAtLocation(currentfield, Real2D(max_x, max_y))
+
+        for i = 1: length(objAtLoc)
+            if objAtLoc[i].state == afd.HOME       #TODO non so se funziona
+                agent.state.reward = afd.afReward
+                agent.state.hasFoodItem = !agent.state.hasFoodItem
+            end
         end
+        
     else
-        max = IMPOSSIBLY_BAD_PHEROMONE
+        max = afd.IMPOSSIBLY_BAD_PHEROMONE
         max_x = x
         max_y = y
         count = 2
@@ -134,14 +142,14 @@ function act(state::SimState, agent::Agent)
 
                 if (dx == 0 & dy == 0) ||
                         _x < 0 || _y < 0 ||
-                        _x >= width || _y >= height
+                        _x >= currentfield.width || _y >= currentfield.height
                     continue
                 end
-                m = toFoodGrid[_x, _y]
+                m = afd.toFoodGrid[_x, _y]
                 if m > max
                     count = 2
                 end
-                if m > max || (m == max && rand(Bool))           #TODO SRISOLVERE RANDOM (rand(Bool) è provvisorio)
+                if m > max || (m == max && rand(Bool))           #TODO RISOLVERE RANDOM (rand(Bool) è provvisorio)
                     max = m
                     max_x = _x
                     max_y = _y
@@ -149,40 +157,48 @@ function act(state::SimState, agent::Agent)
             end
         end
         if max == 0 && agent.state.lastPos != nothing              #nowhere to go!
-            if rand(Bool)           #TODO SRISOLVERE RANDOM (rand(Bool) è provvisorio)
-                xm = x + (x - lastPos.x)
-                ym = y + (y - lastPos.y)
+            if rand(Bool)           #TODO RISOLVERE RANDOM (rand(Bool) è provvisorio)
+                xm = x + (x - agent.state.lastPos.x)
+                ym = y + (y - agent.state.lastPos.y)
 
-                if xm >= 0 && xm < width && ym >= 0 && ym < height     #aggiungere ostacoli in futuro
+                if xm >= 0 && xm < currentfield.width && ym >= 0 && ym < currentfield.height     #aggiungere ostacoli in futuro
                     max_x = xm
                     max_y = ym
                 end
             end
 
-        elseif rand(Bool)           #TODO SRISOLVERE RANDOM (rand(Bool) è provvisorio)
+        elseif rand(Bool)           #TODO SISOLVERE RANDOM (rand(Bool) è provvisorio)
             xd = rand(-1:1)
             yd = rand(-1:1)
             xm = x + xd
             ym = y + yd
-            if !(xd == 0 && yd == 0) && xm >= 0 && xm <= width && ym >= 0 && ym < height
+            if !(xd == 0 && yd == 0) && xm >= 0 && xm <= currentfield.width && ym >= 0 && ym < currentfield.height
                 max_x = xm;
                 max_y = ym;
             end
         end
-        setObjectLocation!(field, agent, Real2D(max_x, max_y))
+        setObjectLocation!(currentfield, agent, Real2D(max_x, max_y))
 
-        if getObjectsAtLocation(field, Real2D(max_x, max_y)) == FOOD       #TODO manca if
-            reward = afReward
-            hasFoodItem = !hasFoodItem
+        objAtLoc = getObjectsAtLocation(currentfield, Real2D(max_x, max_y))
+
+        for i = 1: length(objAtLoc)
+            if objAtLoc[i].state == afd.FOOD       #TODO non so se funziona
+                agent.state.reward = afd.afReward
+                agent.state.hasFoodItem = !agent.state.hasFoodItem
+            end
         end
     end
-    lastPos = location
+    agent.state.lastPos = location
 end
 
 
-function fstep(state::SimState, agent::Agent)
-    depositPheromone(state::SimState, agent::Agent)
-    act(state::SimState, agent::Agent)
+function fstep(state::SimState, agent::Agent, afd::AntsForageData)
+
+    afd.toFoodGrid *= evaporationConstant
+    afd.toHomeGrid *= evaporationConstant
+
+    depositPheromone(state::SimState, agent::Agent, afd::AntsForageData)
+    act(state::SimState, agent::Agent, afd::AntsForageData)
 end
 
 
@@ -191,8 +207,8 @@ mutable struct AntData
     pos::Real2D
     reward::Float64
     hasFoodItem::Bool
-    lastPos::Real2D = nothing
-    AntData(name, pos, reward, hasFoodItem) = new(name, pos, reward, hasFoodItem)
+    lastPos::Real2D
+    #AntData(name, pos, reward, hasFoodItem) = new(name, pos, reward, hasFoodItem)
 
 end
 
--- models/antsforage/antsForages.jl
+++ models/antsforage/antsForages.jl
@@ -3,6 +3,7 @@ using Revise
 using Distributions
 using BenchmarkTools
 using Base
+using Parameters
 include("ants.jl")
 
 
@@ -10,7 +11,7 @@ simstate = SimState()
 myschedule = Schedule(simstate)
 width = 150
 height = 150
-neighborhood_distance = 0.1
+neighborhood_distance = 10
 
 global field = Field2D(width,height,neighborhood_distance/1.5,false)
 
@@ -23,20 +24,20 @@ global const FOOD_X = width - 15.0
 global const FOOD_Y = height - 15.0
 
 #values PHEROMONE
-global const IMPOSSIBLY_BAD_PHEROMONE = -1
-global const LIKELY_MAX_PHEROMONE = 3
+"""global const IMPOSSIBLY_BAD_PHEROMONE = -1
+global const LIKELY_MAX_PHEROMONE = 3"""
 
 #POINT HOME AND FOOD
 posHome = Real2D(HOME_X, HOME_Y)
 posFood = Real2D(FOOD_X, FOOD_Y)
 
 #MATRICES
-toFoodGrid = zeros(Int64, height, width)
-toHomeGrid = zeros(Int64, height, width)
+"""toFoodGrid = zeros(Int64, height, width)
+   toHomeGrid = zeros(Int64, height, width)"""
 
 #values SITES
-global const HOME = 1
-global const FOOD = 2
+"""global const HOME = 1
+global const FOOD = 2"""
 
 patchHome = Patch(HOME)
 patchFood = Patch(FOOD)
@@ -45,14 +46,14 @@ setObjectLocation!(field, patchHome, posHome)
 setObjectLocation!(field, patchFood, posFood)
 
 numAnts = 10000
-evaporationConstant = 0.999
+"""evaporationConstant = 0.999
 afReward = 1.0
 updateCutDown = 0.9
-diagonalCutDown = updateCutDown^√2
+diagonalCutDown = updateCutDown^√2"""
 
 #PROBABILITY
-momentumProbability = 0.8
-randomActionProbability = 0.1
+"""momentumProbability = 0.8
+randomActionProbability = 0.1"""
 
 addfield!(simstate,field)
 
@@ -60,10 +61,28 @@ pos = Real2D(HOME_X, HOME_Y)
 initialReward = 0.0
 
 for i in 1:numAnts
-    a = AntData("Ant", pos, initialReward, false)
+    a = AntData("Ant", pos, initialReward, false, posHome)
     ant = Agent(fstep,a)
     setObjectLocation!(field, ant, pos)
     scheduleRepeating!(myschedule,ant)
 end
 
 @time  simulate!(myschedule,10);
+
+@with_kw struct AntsForageData
+    HOME::Int8 = 1
+    FOOD::Int8 = 2
+    updateCutDown::Float64 = 0.9
+    diagonalCutDown::Float64 = updateCutDown^√2
+    evaporationConstant::Float64 = 0.999
+    momentumProbability::Float64 = 0.8
+    randomActionProbability::Float64 = 0.1
+    afReward::Float64 = 1.0
+
+    IMPOSSIBLY_BAD_PHEROMONE::Int8 = -1
+    LIKELY_MAX_PHEROMONE::Int8 = 3
+
+    toFoodGrid = zeros(Int64, height, width)
+    toHomeGrid = zeros(Int64, height, width)
+
+end
