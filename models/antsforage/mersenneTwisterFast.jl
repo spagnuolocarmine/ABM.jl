1dda462c4186e7f0e83c720f3e30f108d24edc9c
amrlnic <amoriello.nicola97@gmail.com>
2019-10-23 10:33:38 +0200

    work on numberGenerator

--- models/antsforage/mersenneTwisterFast.jl
+++ models/antsforage/mersenneTwisterFast.jl
@@ -12,16 +12,16 @@ global const MATRIX_A = 0x9908b0df
 global const TEMPERING_MASK_B = 0x9d2c5680
 global const TEMPERING_MASK_C = 0xefc60000
 
+global _nextNextGaussian
+global _haveNextNextGaussian
 
 
 global mt = []
 
 global mag01 = []
-
-seed = trunc(Int, rand()*2135454343)
-
-
-function setSeed(seed::Int64)
+array = []
+array = [0x123, 0x234, 0x345, 0x456]
+function setSeed(seed::Int128)
 
     push!(mag01, 0x0)
     push!(mag01, MATRIX_A)
@@ -38,13 +38,48 @@ function setSeed(seed::Int64)
     return mtiLocal-1
 end
 
-global mtiGlobal = setSeed(seed)
+function setSeed(array::Any)
+    seed::Int128 = 19650218
+    setSeed(seed)
+    i = 2
+    j = 1
+    k = (N > length(array) ? N : length(array))
+    while k != 1
+        mt[i] = (mt[i]  ⊻ ((mt[i-1] ⊻ (mt[i-1] >>> 30)) * 1664525)) + array[j] + j
+        j+=1
+        i+=1
+        if i > N
+            mt[1] = mt[N]
+            i = 2
+        end
+        if j > length(array)
+            j = 1
+        end
+
+        k-=1
+    end
+    for k = N:-1:1
+        mt[i] = (mt[i]  ⊻ ((mt[i-1] ⊻ (mt[i-1] >>> 30)) * 1566083941)) - i
+        i+=1
+
+        if i >= N
+            mt[1] = mt[N]
+            i = 2
+        end
+    end
+    mt[1] = 0x80000000
+end
+
+global mtiGlobal = setSeed(array)
+
+
+
 
 
 
 function nextBoolean(probability::Float64)
-    y::Int64 = 0
-    z::Int64 = 0
+    y::Int64 = 1
+    z::Int64 = 1
     mtiLocal = mtiGlobal
 
     if probability < 0.0 || probability > 1.0
@@ -64,24 +99,23 @@ function nextBoolean(probability::Float64)
 
         while kk < (N-M)+1
             y = (mtLocal[kk] & UPPER_MASK) | (mtLocal[kk+1] & LOWER_MASK)
-            mtLocal[kk] = mtLocal[kk+M] ⊻ (y >>> 1) ⊻ mag01Local[y & 0x1]
+            mtLocal[kk] = mtLocal[kk+M] ⊻ (y >>> 1) ⊻ mag01Local[(y & 0x1 == 0) ? 1 : 2]
             kk += 1
         end
         kk2 = kk
         while kk2 < N
             y = (mtLocal[kk2] & UPPER_MASK) | (mtLocal[kk2+1] & LOWER_MASK)
-            mtLocal[kk2] = mtLocal[kk2+(M-N)] ⊻ (y >>> 1) ⊻ mag01Local[y & 0x1]
+            mtLocal[kk2] = mtLocal[kk2+(M-N)] ⊻ (y >>> 1) ⊻ mag01Local[(y & 0x1 == 0) ? 1 : 2]
             kk2 += 1
         end
 
-        y = (mtLocal[N-1] & UPPER_MASK) | (mtLocal[0] & LOWER_MASK)
-        mtLocal[N-1] = mtLocal[M-1] ⊻ (y >>> 1) ⊻ mag01Local[y & 0x1]
+        y = (mtLocal[N-1] & UPPER_MASK) | (mtLocal[1] & LOWER_MASK)
+        mtLocal[N-1] = mtLocal[M-1] ⊻ (y >>> 1) ⊻ mag01Local[(y & 0x1 == 0) ? 1 : 2]
 
-        mtiLocal = 0
+        mtiLocal = 1
     end
 
-    y = mt[mtiLocal]
-    mtiLocal+=1
+    y = mt[(mtiLocal+=1)]
     y ⊻= y >>> 11
     y ⊻= (y << 7) & TEMPERING_MASK_B
     y ⊻= (y << 15) & TEMPERING_MASK_C
@@ -108,8 +142,7 @@ function nextBoolean(probability::Float64)
         mtiLocal = 0
     end
 
-    z = mt[mtiLocal]
-    mtiLocal+=1
+    z = mt[(mtiLocal+=1)]
     z ⊻= z >>> 11
     z ⊻= (z << 7) & TEMPERING_MASK_B
     z ⊻= (z << 15) & TEMPERING_MASK_C
